b0c553cdf2aae6fb2ee7b0b2c69120fdc6e609f5
Xib3rR4dAr <systemsecur8y178@gmail.com>
2020-07-04 20:25:10 +0500

    Create triangle-1.php

---
+++ triangle-series/triangle-1.php
@@ -0,0 +1,21 @@
+<?php
+
+if( 'cli' == php_sapi_name() ){
+	$lineBreak = "\n";
+} else {
+	$lineBreak = "<br>";
+}
+
+?>
+<?php
+
+$lines = 10;
+
+for($i=1, $k=1; $i<=$lines; $i++){
+	for($j=1; $j<$i+1; $j++){
+		echo ( $i & 1 ? $j : $j+1 ) . " ";
+	}
+	echo $lineBreak;
+}
+
+?>
